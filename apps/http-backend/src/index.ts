3fad9aa275441456c2ae4a29ebb0ed13615b22d8
bharat1704 <bharatmali1704@gmail.com>
2025-01-20 18:26:49 +0530

    added db schema in http backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,23 +2,38 @@ import express, { Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/types" 
+import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/types"; 
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup", (req, res)=> {
-//db call
-const data = CreateUserSchema.safeParse(req.body)
-if(!data.success){
-res.json({
-    msg:"incorrect input"
+app.post("/signup", async (req, res)=> {
+    // use zod login
+    const parsedData = CreateUserSchema.safeParse(req.body)
+    if(!parsedData.success){
+        res.json({ msg:"incorrect input" })
+    return;
+    }
+
+    try {
+        await prismaClient.user.create({
+            data:{
+            email: parsedData.data?.username,
+            password: parsedData.data.password,
+            name: parsedData.data.name
+                } 
+        })
+
+        res.json({userId: "123"})
+    } catch(e){
+        res.status(411).json({ msg:"user already exists" })
+
+    }
+
 })
-return;
-}
- res.json({ userId: "123" })
-})
+
 
 //sign in end point
 
