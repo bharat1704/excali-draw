defec31897a038365d54a64e23076aa7fbeb6ee7
bharat1704 <bharatmali1704@gmail.com>
2025-01-22 22:05:38 +0530

    updated http

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -89,4 +89,18 @@ try{
 })
 
 
+app.get("/chats/:roomId", async (req,res)=>{
+    const roomId = Number(req.params.roomId);
+    const messages = await prismaClient.chat.findMany({
+        where:{
+            roomId:roomId
+        },
+        orderBy :{
+            id:"desc"
+        },
+        take:50    
+        })
+     res.json({messages})
+})
+
 app.listen(3006);
